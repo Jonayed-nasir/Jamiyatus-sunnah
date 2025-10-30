5674ef6ebc7532a9a2562e4b9aec67a9981dfcc5
jonayed-nasir <junayednasir073@gmail.com>
2025-10-30 22:02:19 +0600

    loading update 1.3

--- app/loading.js
+++ app/loading.js
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import SpinNer from './components/Spinner'
-export default function loading() {
+export default function Loading() {
   return (
     <div className='flex items-center  justify-center w-full h-screen'>
       <h2 className='text-4xl'>Loading...</h2>
